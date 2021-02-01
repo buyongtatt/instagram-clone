c45bfe89c1a1a79ccff73e658b54c07dad8fbbb6
021120 <yongtatt981213@hotmail.com>
2021-02-01 14:09:54 +0800

    Header

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Post from "./Post";
 function App() {
   return (
     <div className="App">
-      
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
-          width='103px'
-          height='29px'
+          width="103px"
+          height="29px"
           alt=""
         />
       </div>
       <h1>Hello World🚀🚀</h1>
+      <Post
+        username="buyongtatt"
+        caption="wow"
+        imageUrl="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+      />
+
       <Post />
-      
+      <Post />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Post.css";
 
 function Post() {
-    return (
-        <div className='post'>
-            <h3>Username</h3>
-            {/* header -> avatar + username */}
-            <img src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt=""/>
-            {/* image */}
-            {/* username + caption */}
+  return (
+    <div className="post">
+        <div className="post__header">
+        <Avatar 
+        className='post__avatar'
+        alt='Bu Yong Tatt'
+        src='/static/images/avatar/1.jpg'
+        />
+      <h3>Username</h3>
         </div>
-    )
+        
+      {/* header -> avatar + username */}
+      <img
+        className="post__image"
+        src="https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+        alt=""
+      />
+      {/* image */}
+      <h4 className="post__text">
+        <strong>Username</strong> caption
+      </h4>
+      {/* username + caption */}
+    </div>
+  );
 }
 
-export default Post
+export default Post;
